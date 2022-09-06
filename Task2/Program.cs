00736b6ebb907aea1f282b77981ff7191cee6528
«Antonina <a1523@mail.ru>
2022-09-06 21:36:39 +0300

    Task1

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+void Task2(string number)
+{
+if (number.Length > 2)
+{
+    Console.WriteLine($"Третья цифра числа {number[2]}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры в числе нет");
+}
+}
+
+Console.WriteLine("Введите число: ");
+string number = Convert.ToString(Console.ReadLine());
+Task2(number);
