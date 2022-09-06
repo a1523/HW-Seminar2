4298085bf1767f72bbe944f9ac1ab5dd6c1e4355
«Antonina <a1523@mail.ru>
2022-09-06 19:59:59 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void Task1(int numb)
+{
+    int result = (numb/10)%10;
+    Console.WriteLine(result);
+}
+
+Console.WriteLine("Введите трехзначное число: ");
+int x = int.Parse(Console.ReadLine());
+Task1(x);
